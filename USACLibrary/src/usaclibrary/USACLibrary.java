433b9164662d519ca7865781455f88ecd120dd05
ChrisUSAC <criscobar02@gmail.com>
2020-05-13 02:36:45 -0600

    interfaces

---
+++ USACLibrary/src/usaclibrary/USACLibrary.java
@@ -0,0 +1,19 @@
+package usaclibrary;
+
+import interfaz.Login;
+
+/**
+ *
+ * @author cris
+ */
+public class USACLibrary {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new Login().setVisible(true);
+    }
+
+}
